82d23c0877af49663ca6aa7b04ce68faed7b473a
Colter Garrison <102547626+Colter-Garrison@users.noreply.github.com>
2022-08-11 15:41:23 -0700

    first test passing. login and redirect to github dashboard

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,15 +1,30 @@
 const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
-// const request = require('supertest');
-// const app = require('../lib/app');
+const request = require('supertest');
+const app = require('../lib/app');
 
-describe('backend-express-template routes', () => {
+jest.mock('../lib/services/github');
+
+describe('OAuth routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('GET should redirect to Githubs OAuth for login', async () => {
+  it('GET should redirect to GitHub OAuth for login', async () => {
+    const resp = await request
+      .agent(app)
+      .get('/api/v1/github/callback?code=42')
+      .redirects(1);
 
-  })
+    expect(resp.body).toEqual({
+      id: expect.any(String),
+      username: 'user',
+      email: 'user@me.com',
+      avatar: expect.any(String),
+      iat: expect.any(Number),
+      exp: expect.any(Number),
+    });
+  });
+  
   afterAll(() => {
     pool.end();
   });

0aab738659750bf647c25f62a68ff36c9ba9e446
Colter Garrison <102547626+Colter-Garrison@users.noreply.github.com>
2022-08-12 14:10:15 -0700

    GET test to view a list of posts is passing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const agent = request.agent(app);
 
 jest.mock('../lib/services/github');
 
@@ -24,10 +25,20 @@ describe('OAuth routes', () => {
       exp: expect.any(Number),
     });
   });
-  
+
   it('DELETE should log out a user', async () => {
     const resp = await request(app).delete('/api/v1/github/sessions');
     expect(resp.status).toBe(200);
+  });
+
+  it ('GET should allow authenticated users to view a list of posts', async () => {
+    await agent
+      .get('/api/v1/github/callback?code=42');
+    const resp = await agent.get('/api/v1/posts');
+    expect(resp.body).toEqual(expect.arrayContaining([{
+      id: expect.any(String),
+      posts: expect.any(String),
+    }]));
   });
 
   afterAll(() => {
